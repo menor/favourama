873ac23427320d6e903e22658d243f9a2d6f1f31
menor <josemenor@gmail.com>
2020-02-06 19:29:20 +0100

    feat(main): implement functionality to favourite episodes
    Not sure if this is the most performant way or I should modify the episodes list coming from the
    server

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,67 +1,25 @@
 import React, { useState, useEffect } from 'react'
+import { xor } from 'lodash/fp'
 import FuturamaApi from '../apis'
 import Header from './Header'
-
-type EpisodeImage = {
-  medium: string
-  original: string
-}
-
-type EpisodeSelfLink = {
-  href: string
-}
-
-type EpisodeLink = {
-  self: EpisodeSelfLink
-}
-
-type Episode = {
-  id: number
-  url: string
-  name: string
-  season: number
-  number: number
-  airdate: string
-  airtime: string
-  airstamp: string
-  runtime: number
-  image: EpisodeImage
-  summary: string
-  _links: EpisodeLink
-}
-
-type EpisodeListProps = {
-  items: Episode[]
-}
-
-// We need to wrap this list inside a fragment until the issue is solved
-// in definitivleyTyped
-// https://github.com/DefinitelyTyped/DefinitelyTyped/issues/20356
-const EpisodeList = (props: EpisodeListProps) => {
-  if (props.items) {
-    return (
-      <>
-        {' '}
-        {props.items.map((e: Episode) => (
-          <section key={e.id}>
-            <img src={e.image.medium} alt={`Futurama - ${e.name}`} />
-            <div className="meta">
-              <p>{e.name}</p>
-              <p>{`S${e.season}E${e.number}`}</p>
-            </div>
-          </section>
-        ))}
-      </>
-    )
-  }
-  return null
-}
+import EpisodeList, { TEpisode } from './EpisodeList'
 
 const App = () => {
-  const [episodes, setEpisodes] = useState<Episode[] | []>([])
+  const [episodes, setEpisodes] = useState<TEpisode[] | []>([])
+  const [favourites, setFavourite] = useState<Number[]>([])
+
+  const onFav = (fav: number) => {
+    setFavourite(prev => {
+      return xor(prev, [fav])
+    })
+  }
+
+  const isFav = (id: number) => {
+    return favourites.includes(id)
+  }
 
   async function fetchData() {
-    const episodes: Episode[] = await FuturamaApi.get('episodes').json()
+    const episodes: TEpisode[] = await FuturamaApi.get('episodes').json()
     setEpisodes(episodes)
   }
 
@@ -72,7 +30,7 @@ const App = () => {
   return (
     <div className="sans-serif">
       <Header />
-      <EpisodeList items={episodes} />
+      <EpisodeList isFav={isFav} onFav={onFav} items={episodes} />
     </div>
   )
 }
