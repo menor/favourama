2c1982ff22b218b9d826cf54f53ee196d36a6891
menor <josemenor@gmail.com>
2020-02-07 20:06:48 +0100

    feat(styles): improve header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,24 @@
 import React from 'react'
 
+// TODO next:
+// style and function favs unfavs
+// Improve cards typography
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <section>
-        <h1 className="mv0 normal">Favourama</h1>
-        <p className="mv1">Favourite futurama episodes</p>
-      </section>
+      <h1 className="lh-solid mv0 tracked-tight bold dark-gray">Favourama</h1>
+      <p className="mv1 silver">Favourite futurama episodes</p>
+      <p>favs | unfavs</p>
     </header>
   )
 }
 
 const styles = {
   header: `
-    pa3
+    pt3
+    ph4
+    pb4
     sans-serif
-    bg-light-blue
 `
 }
 
