829cba96d9583868626c7f7b5fafb88bc725a2ae
menor <josemenor@gmail.com>
2020-02-14 20:51:21 +0100

    feat(filtering): add basic filtering
    Still does not feel like the most elegant solution, but it works

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, MouseEvent } from 'react'
 import { xor } from 'lodash/fp'
 import FuturamaApi from '../apis'
 import Header from './Header'
@@ -7,11 +7,45 @@ import EpisodeList, { TEpisode } from './EpisodeList'
 const App = () => {
   const [episodes, setEpisodes] = useState<TEpisode[] | []>([])
   const [favourites, setFavourite] = useState<Number[]>([])
+  const [showFavs, setShowFavs] = useState<boolean>(true)
+  const [showUnfavs, setShowUnfavs] = useState<boolean>(true)
+
+  const showFavouriteEpisodes = () => {
+    return episodes.filter(e => favourites.includes(e.id))
+  }
+
+  const showUnfavouriteEpisodes = () => {
+    return episodes.filter(e => !favourites.includes(e.id))
+  }
+
+  const getVisibleEpisodes = () => {
+    if (showFavs && showUnfavs) {
+      return episodes
+    }
+
+    if (showFavs) {
+      return showFavouriteEpisodes()
+    }
+
+    if (showUnfavs) {
+      return showUnfavouriteEpisodes()
+    }
+
+    return episodes
+  }
 
   const onFav = (fav: number) => {
     setFavourite(prev => {
       return xor(prev, [fav])
     })
+  }
+
+  const onFavFilterClick = (e: MouseEvent) => {
+    setShowFavs(prev => !prev)
+  }
+
+  const onUnfavFilterClick = (e: MouseEvent) => {
+    setShowUnfavs(prev => !prev)
   }
 
   const isFav = (id: number) => {
@@ -29,8 +63,13 @@ const App = () => {
 
   return (
     <div className="sans-serif">
-      <Header />
-      <EpisodeList isFav={isFav} onFav={onFav} items={episodes} />
+      <Header
+        favFilterActive={showFavs}
+        unfavFilterActive={showUnfavs}
+        onFavFilterClick={onFavFilterClick}
+        onUnfavFilterClick={onUnfavFilterClick}
+      />
+      <EpisodeList isFav={isFav} onFav={onFav} items={getVisibleEpisodes()} />
     </div>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,23 +3,68 @@ import React from 'react'
 // TODO next:
 // style and function favs unfavs
 // Improve cards typography
-const Header: React.FC = () => {
+//
+
+type HeaderProps = {
+  favFilterActive: boolean
+  unfavFilterActive: boolean
+  onFavFilterClick: Function
+  onUnfavFilterClick: Function
+}
+
+const Header = (props: HeaderProps) => {
   return (
     <header className={styles.header}>
       <h1 className="lh-solid mv0 tracked-tight bold dark-gray">Favourama</h1>
       <p className="mv1 silver">Favourite futurama episodes</p>
-      <p>favs | unfavs</p>
+      <p className="tr mt3">
+        <span className="f6 ttu silver">show: </span>
+        <button
+          className={styles.filterButton({ active: props.favFilterActive })}
+          onClick={e => props.onFavFilterClick(e)}
+        >
+          favs
+        </button>
+        <button
+          className={styles.filterButton({ active: props.unfavFilterActive })}
+          onClick={e => props.onUnfavFilterClick(e)}
+        >
+          unfavs
+        </button>
+      </p>
     </header>
   )
+}
+
+type filterButtonProps = {
+  active: boolean
 }
 
 const styles = {
   header: `
     pt3
     ph4
-    pb4
+    pb1
+    mb1
     sans-serif
-`
+    br4
+    br--bottom
+`,
+  filterButton: ({ active }: filterButtonProps) => `
+    f6
+    link
+    dim
+    br2
+    ba
+    ph2
+    pv2
+    mb2
+    ml2
+    dib
+    pointer:hover
+    ${active ? 'white' : 'orange'}
+    ${active ? 'bg-orange' : 'bg-white'}
+  `
 }
 
 export default Header
