8ace5a364dcecb07f8c20d40f3ecdb66e0d9d583
menor <josemenor@gmail.com>
2020-03-10 11:17:26 +0100

    chore: move EpisodeList to features

--- src/apis/index.js
+++
@@ -1,6 +0,0 @@
-import ky from 'ky'
-
-export default ky.create({
-  prefixUrl: 'http://api.tvmaze.com/shows/538'
-})
-
---
+++ src/features/EpisodeList/EpisodeList.tsx
@@ -0,0 +1,105 @@
+import React from 'react'
+import { TEpisode } from '../../api/tvmazeAPI'
+import Heart from '../../components/Heart'
+import Screenshot from '../../components/Screenshot'
+
+// We need to wrap this list inside a fragment until the issue is solved
+// in definitivleyTyped
+// https://github.com/DefinitelyTyped/DefinitelyTyped/issues/20356
+const EpisodeList = (props: EpisodeListProps) => {
+  if (props.items.length > 0) {
+    return (
+      <section className={st.episodeList}>
+        {' '}
+        {props.items.map((e: TEpisode) => {
+          return (
+            <article className={st.card} key={e.id}>
+              <div className={st.innerCard}>
+                <Screenshot src={e.image.medium} alt={`Futurama - ${e.name}`} />
+                <div className={st.meta}>
+                  <h1 className={st.title}>{e.name}</h1>
+                  <h2 className={st.episodeNumber}>{`S${e.season}E${
+                    e.number
+                  }`}</h2>
+                </div>
+                <div className={st.like}>
+                  <span
+                    onClick={() => {
+                      props.onFav(e.id)
+                    }}
+                  >
+                    <Heart solid={props.isFav(e.id)} />
+                  </span>
+                </div>
+              </div>
+            </article>
+          )
+        })}
+      </section>
+    )
+  }
+  return (
+    <section className={st.episodeList}>
+      <p className={st.noEpisodes}>No episodes matching your filter(s)</p>
+    </section>
+  )
+}
+
+const st = {
+  card: `
+    pa1
+    w-100
+    w-50-m
+    w-25-ns
+    mb2
+`,
+  innerCard: `
+    flex
+    flex-wrap
+    pa2
+    ba
+    b--light-gray
+    shadow-hover
+  `,
+  episodeList: `
+    flex
+    flex-wrap
+    w-100
+    pa3
+  `,
+  meta: `
+    mt2
+    w-90
+  `,
+  title: `
+    f4
+    fw4
+    mv1
+  `,
+  episodeNumber: `
+    f5
+    fw4
+    mt0
+  `,
+  like: `
+   w-10 
+   mt3
+   flex
+   items-end
+  `,
+  noEpisodes: `
+    center
+    f3
+    pt6
+    silver
+  `
+}
+
+type EpisodeListProps = {
+  items: TEpisode[]
+  isFav: Function
+  onFav: Function
+}
+
+export default EpisodeList
+
