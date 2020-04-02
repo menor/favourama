61072bfe676787ad1cec3c12fc65b85b6d1b5e67
menor <josemenor@gmail.com>
2020-04-02 10:14:51 +0200

    Updates

---
+++ precache-manifest.99fb5c98bcf6460c2b1836272369f4f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "385bb4588e6dc9efc87cca4bf2943adf",
+    "url": "/menor/favourama/index.html"
+  },
+  {
+    "revision": "2b1becc7997b50b3c045",
+    "url": "/menor/favourama/static/css/main.4eda9ebe.chunk.css"
+  },
+  {
+    "revision": "6f5c617fa9425384d573",
+    "url": "/menor/favourama/static/js/2.94038aa4.chunk.js"
+  },
+  {
+    "revision": "777717e812b6c6eb53ffe306ffcd94c6",
+    "url": "/menor/favourama/static/js/2.94038aa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b1becc7997b50b3c045",
+    "url": "/menor/favourama/static/js/main.8af5e775.chunk.js"
+  },
+  {
+    "revision": "5f828185db6c36837703",
+    "url": "/menor/favourama/static/js/runtime-main.9138514e.js"
+  }
+]);
